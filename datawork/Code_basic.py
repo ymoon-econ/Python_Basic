ac8c0d68fc47465a379e7964d19e115df91cef3d
ymoon-econ <32029208+ymoon-econ@users.noreply.github.com>
2018-02-17 11:27:10 -0600

    Update Code_basic.py

--- datawork/Code_basic.py
+++ datawork/Code_basic.py
@@ -20,3 +20,23 @@ def read_csv(csvdata):
             int_fields.append(int(j))
         final_list.append(int_fields)
     return final_list
+
+# Tabulate in dictionary form
+def month_births(List):
+    births_per_month = {}
+    for i in List:
+        if i[1] in births_per_month:
+            births_per_month[i[1]] += i[4]
+        else:
+            births_per_month[i[1]] = i[4]
+    return births_per_month
+
+# More generally 
+def calc_counts(Data, Col):
+    Birth = {}
+    for i in Data:
+        if i[Col] in Birth:
+            Birth[i[Col]] += i[4]
+        else:
+            Birth[i[Col]] = i[4]
+    return Birth

529ae6e490da360f733f1699420aaa4797c11361
ymoon-econ <32029208+ymoon-econ@users.noreply.github.com>
2018-02-17 11:17:35 -0600

    Update Code_basic.py

--- datawork/Code_basic.py
+++ datawork/Code_basic.py
@@ -2,3 +2,21 @@
 f = open('US_births_1994-2003_CDC_NCHS.csv','r')
 f = f.read()
 rawdata = f.split('\n')
+
+# purge the label
+data = rawdata[1:]
+
+# create function which read csvdata and produce inter list
+def read_csv(csvdata):
+    temp = open(csvdata,'r')
+    temp = temp.read()
+    temp = temp.split('\n')
+    string_list = temp[1:]
+    final_list = []
+    for i in string_list:
+        int_fields = []
+        string_fields = i.split(',')
+        for j in string_fields:
+            int_fields.append(int(j))
+        final_list.append(int_fields)
+    return final_list

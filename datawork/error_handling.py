c570616260c178990cd80e0a2675648f96cb1677
ymoon-econ <32029208+ymoon-econ@users.noreply.github.com>
2018-02-25 13:57:28 -0600

    Create error_handling.py

---
+++ datawork/error_handling.py
@@ -0,0 +1,12 @@
+# Run the code like
+int('') # code will be terminated here so that the next line is not executed.
+1+1  
+
+
+# try this
+try:
+    int('')
+except Exception:
+    print('Error!')
+1+1
+    

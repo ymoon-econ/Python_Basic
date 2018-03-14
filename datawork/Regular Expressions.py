c6f2c44abeb7754895254a024ceda676cc5224cd
ymoon-econ <32029208+ymoon-econ@users.noreply.github.com>
2018-03-14 11:03:35 -0500

    Create Regular Expressions.py

---
+++ datawork/Regular Expressions.py
@@ -0,0 +1,5 @@
+# "^a" will match all strings that start with "a"
+# "a$" will match all strings that end with "a"
+
+# We can account for this inconsistency with square brackets. We use square brackets in a regex to indicate that any character within them can fill the space.
+

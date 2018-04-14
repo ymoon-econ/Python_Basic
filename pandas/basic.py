94b9b6f55ad10574ad20433aa2d550c5c5e2f4a9
ymoon-econ <32029208+ymoon-econ@users.noreply.github.com>
2018-04-14 12:53:51 -0500

    Update basic.py

--- pandas/basic.py
+++ pandas/basic.py
@@ -1,6 +1,11 @@
+# Introduction to Pandas
+# 10-13
+
 test=food_info.columns.tolist()
 gram_columns =[]
 for i in test:
     if i.endswith("(g)"):
         gram_columns.append(i)
 gram_df = food_info[gram_columns]
+
+
